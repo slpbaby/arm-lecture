7973b5cffff8e92061a7b02b7d28a13a3185b225
slp-baby <slp-baby@hotmail.com>
2014-09-26 14:11:50 +0800

    lab1: use conditional execution, but didn't improve the implementation

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -16,12 +16,16 @@ fibonacci:
 	@R4 = R0 - 0 (update flags)
 	@ if(R0 <= 0) goto .L3 (which returns 0)
 	subs r4, r0, #0
-	ble .L3
+	itt eq
+	moveq r0, #0
+	beq .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
 	cmp r4, #1
-	beq .L4
+	itt eq
+	moveq r0, #1
+	beq .L3
 
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
@@ -37,16 +41,9 @@ fibonacci:
 
 	@ R0 = R5 + R0 (update flags)
 	add r0, r5, r0
+.L3:
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
-
 	.size fibonacci, .-fibonacci
 	.end
