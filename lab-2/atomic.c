f4a91e0c128d031ead30068aea61c991e63d890b
slp-baby <slp-baby@hotmail.com>
2014-09-26 14:10:57 +0800

    lab2: implement mutex

--- lab-2/atomic.c
+++ lab-2/atomic.c
@@ -18,17 +18,20 @@ unsigned int mutexlock = unlocked;
 void *doSomeThing(void *arg)
 {
 	// pthread_mutex_lock(&lock);
+	printf("\n Try lock\n");
 	lock_mutex(&mutexlock); 
+	printf("\n Lock done\n");
 	unsigned long i = 0;
 	counter += 1;
 	printf("\n Job %d started\n", counter);
 
-	for (i = 0; i < (0xFFFFFFFF); i++);
+	for (i = 0; i < (0xFFFFFF); i++);
 
 	printf("\n Job %d finished\n", counter);
 
 	// pthread_mutex_unlock(&lock);
 	unlock_mutex(&mutexlock);
+	printf("\n Release\n");
 	return NULL;
 }
 
--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,15 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+.L1:
+	ldr r1, =locked
+	ldrex r2, [r0]
+	cmp r2, r1
+	beq .L1
+	strex r2, r1, [r0]
+	cmp r2, #0
+	bne .L1
+	dmb
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +27,11 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+        ldr r4, =unlocked
+	dmb
+	str r4, [r0]
+	dsb
+	sev
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
